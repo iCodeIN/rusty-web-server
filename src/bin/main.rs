1612c5636765986a00becd5cad5ab60affcfd35c
yozhgoor <yozhgoor@outlook.com>
2021-10-25 16:46:15 +0200

    Change the files structure to create a library

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,4 @@
+use rusty_web_server::ThreadPool;
 use std::fs;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
@@ -6,10 +7,12 @@ use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
