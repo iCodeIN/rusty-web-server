84d88d77c31d925b71998a2f9f4825ddaefcc68f
yozhgoor <yozhgoor@outlook.com>
2021-10-25 21:34:16 +0200

    Use an Option for the worker thread

--- src/lib.rs
+++ src/lib.rs
@@ -47,14 +47,16 @@ impl Drop for ThreadPool {
         for worker in &mut self.workers {
             println!("Shutting down worker {}", worker.id);
 
-            worker.thread.join().unwrap();
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
         }
     }
 }
 
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
@@ -67,6 +69,9 @@ impl Worker {
             job();
         });
 
-        Worker { id, thread }
+        Worker {
+            id,
+            thread: Some(thread),
+        }
     }
 }
